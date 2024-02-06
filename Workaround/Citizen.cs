4565cfc168640e5cb2986242740f4befee863cc7
HuseinliAli <ali.guseynoov@gmail.com>
2024-02-06 14:37:00 +0400

    Add project files.

---
+++ Workaround/Citizen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workaround;
+public class Citizen
+{
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public int BirthYear { get; set; }
+    public string NationalIdentityNumber { get; set; }
+}
